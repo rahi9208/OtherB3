c03eec0068f55071779e690ddc6b55629e48cce8
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-14 18:21:35 +0530

    adding translations

--- OtherB3/list.js
+++ OtherB3/list.js
@@ -1,5 +1,6 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
+let translation = new AWS.Translate();
 exports.handler = function (event, context, callback) {
 
 	let response = {
@@ -7,8 +8,8 @@ exports.handler = function (event, context, callback) {
 		"statusCode": 200,
 		"headers": {
 			"Access-Control-Allow-Origin": "*",
-			"MyHeader":"Yo",
-			"Access-Control-Allow-Headers":"*"
+			"MyHeader": "Yo",
+			"Access-Control-Allow-Headers": "*"
 		},
 		"body": "..."
 	};
@@ -18,13 +19,21 @@ exports.handler = function (event, context, callback) {
 
 	ddb.scan({
 		TableName: 'otherb3', ExpressionAttributeValues: { ':it': itemType }, FilterExpression: 'itemType = :it'
-	}, function (err, data) {
+	}, async function (err, data) {
 		if (!err && data.Items) {
-			response.body = Buffer.from(JSON.stringify(data.Items)).toString("base64");
+			response.body = JSON.stringify(await Promise.all(data.Items.map(async (item) => {
+				item.image = "https://s3.amazonaws.com/" + process.env["IMAGES_BUCKET"] + "/" + item.itemCode + ".jpg";
+				item.itemName = await translateName(item.itemName, "zh");
+				return item;
+			})));
 		} else {
 			response.statusCode = 404;
 			response.body = "No items found";
 		}
 		callback(err, response);
 	});
+}
+
+async function translateName(name, to) {
+	return (await translation.translateText({ SourceLanguageCode: "en", TargetLanguageCode: to, Text: name }).promise()).TranslatedText;
 }
